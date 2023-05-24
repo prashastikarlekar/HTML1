210efa2dc0cc4e68cbe28ef70573f119c30624d3
Prashasti Karlekar <prkarl@iu.edu>
2023-05-24 01:04:16 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+// var heading = document.getElementById("heading");
+// heading.classList.add("text-center");
+// heading.innerHTML = `
+// <h1 class="heading">Sign Up</h1>
+// `;
